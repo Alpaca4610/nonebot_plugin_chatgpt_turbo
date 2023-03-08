ea49cba9b3248260a4e42d68f43025727b08ed12
Alpaca <Alpaca4610>
2023-03-08 13:35:23 +0800

    增加可选上下文回复功能

--- nonebot_plugin_chatgpt_turbo/__init__.py
+++ nonebot_plugin_chatgpt_turbo/__init__.py
@@ -1,6 +1,7 @@
 import nonebot
 import os
 import asyncio
+import openai
 
 from nonebot import on_command
 from nonebot.params import CommandArg
@@ -12,9 +13,12 @@ from .ChatSession import ChatSession
 
 try:
     api_key = nonebot.get_driver().config.openai_api_key
-    model_id = nonebot.get_driver().config.openai_model_name
 except:
     api_key = ""
+    
+try:
+    model_id = nonebot.get_driver().config.openai_model_name
+except:
     model_id = "gpt-3.5-turbo"
 
 try:
@@ -34,7 +38,9 @@ if http_proxy != "":
 
 session = {}
 
-chat_request = on_command("", rule=to_me(), block=True, priority=1)
+
+# 带上下文的聊天
+chat_request = on_command("/chat", block=True, priority=1)
 
 
 @chat_request.handle()
@@ -61,6 +67,47 @@ async def _(event: MessageEvent, msg: Message = CommandArg()):
     except Exception as error:
         await chat_request.finish(str(error), at_sender=True)
     await chat_request.finish(MessageSegment.text(res), at_sender=True)
+
+
+# 不带上下文的聊天
+chat_request2 = on_command("", rule=to_me(), block=True, priority=1)
+
+def get_response(content):
+    openai.api_key = api_key
+    res_ = openai.ChatCompletion.create(
+        model=model_id,
+        messages=[
+            {"role": "user", "content": content}
+        ]
+    )
+
+    res = res_.choices[0].message.content
+
+    while res.startswith("\n") != res.startswith("？"):
+        res = res[1:]
+    print(res)
+
+    return res
+
+@chat_request2.handle()
+async def _(event: MessageEvent, msg: Message = CommandArg()):
+    print(event.get_session_id())
+    if api_key == "":
+        await chat_request2.finish(MessageSegment.text("请先配置openai_api_key"))
+
+    content = msg.extract_plain_text()
+    if content == "" or content is None:
+        await chat_request2.finish(MessageSegment.text("内容不能为空！"))
+
+    await chat_request2.send(MessageSegment.text("ChatGPT正在思考中......"))
+
+    loop =  asyncio.get_event_loop()
+    try:
+        res = await loop.run_in_executor(None, get_response, content)
+
+    except Exception as error:
+        await chat_request2.finish(str(error))
+    await chat_request2.finish(MessageSegment.text(res))
 
 
 clear_request = on_command("/clear", block=True, priority=1)
